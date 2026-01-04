42463adabb2e541f1e22ca7cb4f2a940d37575c3
MySecondAttemptToFetchDataFromRemoteToLocal <surajsahu2807@gmail.com>
2026-01-04 20:01:26 +0530

    Lab001_HelloWorld file created

---
+++ src/ex_01_Java_ATB_Practice/Lab001_HelloWorld.java
@@ -0,0 +1,8 @@
+package ex_01_Java_ATB_Practice;
+
+public class Lab001_HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.println("Suraj Sahu");
+    }
+}
