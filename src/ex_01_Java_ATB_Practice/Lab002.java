f2b93cc9d799e0a2f64b5bd3bc4ab8be67172dc7
MySecondAttemptToFetchDataFromRemoteToLocal <surajsahu2807@gmail.com>
2026-01-08 02:01:21 +0530

    10th May Pracice

---
+++ src/ex_01_Java_ATB_Practice/Lab002.java
@@ -0,0 +1,9 @@
+package ex_01_Java_ATB_Practice;
+
+public class Lab002 {
+    public static void main(String[] args) {
+
+        System.out.println("This is an Statement");
+
+    }
+}
---
+++ src/ex_01_Java_ATB_Practice/Lab003_NoMain.java
@@ -0,0 +1,8 @@
+package ex_01_Java_ATB_Practice;
+
+public class Lab003_NoMain {
+
+    /* Yes we can write a program without main method but when you try to run a program
+    then it will not run
+    */
+}
